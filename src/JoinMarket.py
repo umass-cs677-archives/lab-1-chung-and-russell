ec26bb1a13f3b04f218e98d53303a8f1f6e1cfe4
chung <chung.yang@engineer.uconn.edu>
2019-02-26 21:24:54 -0500

    update

--- src/JoinMarket.py
+++ src/JoinMarket.py
@@ -1,6 +1,7 @@
 import configparser
 from Person import Person
 import Pyro4
+import Pyro4.naming
 import re
 import socket
 
@@ -21,13 +22,16 @@ if __name__ == "__main__":
 
     person = Person(id, n_items, goods ,isbuyer)
 
+    name_server_hostname = config["NETWORK_INFO"]["NAME_SERVER_HOST"]
     hostname = socket.gethostname()
+    # Pyro4.naming.startNS(host = name_server_hostname)
+
     with Pyro4.Daemon(host = hostname) as daemon:
         person_uri = daemon.register(person)
         print(daemon.uriFor("BUYER1", nat=False))
         print(daemon.uriFor("BUYER1", nat=False))
 
-        with Pyro4.locateNS(host=hostname) as ns:
+        with Pyro4.locateNS(host=name_server_hostname) as ns:
             ns.register(id, person_uri)
             print(id, "joined the market")
             daemon.requestLoop()
