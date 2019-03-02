d1da99ec9112e48879a1dadc596fb49e5820a541
chung <chung.yang@engineer.uconn.edu>
2019-03-01 21:51:49 -0500

    test event loop

--- src/Person.py
+++ src/Person.py
@@ -31,13 +31,12 @@ class Person(Thread):
         :param ns_dict: a dictionary name-to-URI from a name server
         :return:
         """
-        print(self.id)
         list = []
         # Filters out the NameServer and itself from the registered objects. Only store the actual location of
         # other registered objects
         for id in ns_dict:
             if "NameServer" not in id and self.id != id:
-                list.append(ns_dict[id])
+                list.append(id)
 
         # Randomly pick one neighbor. The number 10 is used to keep the number of times the neighbor responds
         # or contacts
@@ -47,7 +46,7 @@ class Person(Thread):
         hostname = socket.gethostname()
 
 
-        with Pyro4.Daemon(host=hostname) as daemon:
+        with Pyro4.Daemon() as daemon:
             ns = Pyro4.locateNS()
             person_uri = daemon.register(self)
             ns.register(self.id, person_uri)
@@ -68,13 +67,25 @@ class Person(Thread):
 
             #Buyer loop
             while True and self.role == "buyer":
-
-                for neighbor_location in self.neighbors:
-
-                    neighbor = Pyro4.Proxy(neighbor_location)
-                    neighbor.lookup(self.good, 4, self.id)
-
-            # while True and self.role == "seller":
+                #
+                # print(self.id)
+                # for neighbor_location in self.neighbors:
+                #
+                #     try:
+                #         neighbor = Pyro4.Proxy(ns.lookup(neighbor_location))
+                #         neighbor.lookup(self.good, 4, self.id)
+                #
+                #     except(Exception) as e:
+                #         self.neighbors[neighbor_location] -= 1
+                #         if self.neighbors[neighbor_location] <= 0:
+                #             del self.neighbors[neighbor_location]
+                #         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                #         message = template.format(type(e).__name__, e.args)
+                #         print(message)
+                # time.sleep(3)
+                continue
+            while True and self.role == "seller":
+                daemon.requestLoop()
 
 
 
@@ -89,11 +100,7 @@ class Person(Thread):
         :return: The sellers who sell specified product
         """
 
-
-
-
-        return
-
+        print(id, "has a look up from", self.id)
     @Pyro4.expose
     def reply(self, seller_id):
         """
@@ -101,7 +108,7 @@ class Person(Thread):
         :param seller_id:
         :return:
         """
-
+        print("hi")
         return
 
     @Pyro4.expose
@@ -109,6 +116,9 @@ class Person(Thread):
 
         return
 
+    @Pyro4.expose
+    def hi(self):
+        print("hi")
 
 
 
